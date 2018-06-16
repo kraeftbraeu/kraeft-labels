3284a14d5a4cb1a4745657613b62133cce6a8676
mk-84 mint <manuel_kraemer@gmx.net>
2018-06-16 08:44:00 +0200

    initial

---
+++ kraeft.js
@@ -0,0 +1,16 @@
+function copy(m, n, callback) {
+	var original = $("#picture");
+
+	for(var i = 1; i < m; i++) {
+		var clone = original.clone();
+		clone.id = "original" + i;
+		original.parent().append(clone);
+	}
+
+	for(var i = 0; i < n; i++) {
+		var clone = original.clone();
+		clone.id = "changed" + i;
+		callback(clone);
+		original.parent().append(clone);
+	}
+}
